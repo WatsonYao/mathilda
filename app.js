92dd2eff89b7f598e5c4b52a90dad01849d9454e
watson <yaoyuan131617@gmail.com>
2016-05-19 17:49:44 +0800

    update

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('/node_modules/material-design-lite'));
+app.use(express.static('node_modules/material-design-lite'));
 
 app.use('/', routes);
 app.use('/users', users);
