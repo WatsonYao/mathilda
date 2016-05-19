9d451e1ffac002a64ff490413bbaecd1843d4c2c
watson <yaoyuan131617@gmail.com>
2016-05-19 17:29:29 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mathilda' });
+router.get('/', function (req, res, next) {
+    //res.render('index', { title: 'Mathilda' });
+    res.renderFile('/public/index.html')
 });
 
 module.exports = router;
