c607b8d8e7d886e3aece22d35158eb400955b9f8
watson <yaoyuan131617@gmail.com>
2016-05-20 16:20:10 +0800

    update

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/static', express.static(__dirname + '/node_modules/material-design-lite'));
 app.use('/examples', express.static(__dirname + '/examples'));
+app.use('/build', express.static(__dirname + '/build'));
 
 app.use('/', routes);
 app.use('/users', users);
