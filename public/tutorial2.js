6dadeab425a72384e046bf93f9a33b1e47b6480b
watson <yaoyuan131617@gmail.com>
2016-05-20 17:41:35 +0800

    update

---
+++ public/tutorial2.js
@@ -0,0 +1,39 @@
+// tutorial2.js
+var CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="commentList">
+                Hello, world! I am a CommentList.
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
+//Next, update the CommentBox component to use these new components:
+
+// tutorial3.js
+var CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
